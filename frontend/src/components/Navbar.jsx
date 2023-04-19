22c1316fee8f2a710861a85ee98a1492d7962606
Shkelqim Cakaj <shkelqimcakaj@hotmail.com>
2023-04-19 14:39:26 +0200

    navbar with tailwind and some tailwind config

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <div className="container flex justify-between px-20 h-20 items-center shadow-md bg-white" >
+    <div className="container flex justify-between px-20 h-20 items-center shadow-md bg-[--background]" >
         <div className="navbarLeftSide flex flex-row items-center">
           
             <h1 className='text-3xl pr-20'>Logo</h1>
-            <Link to="/" className='px-20 text-blue-500 font-semibold text-lg'>Features</Link>
-            <Link to="/" className='text-blue-500 font-semibold text-lg'>Get in Touch</Link>
+            <Link to="/" className='px-20 text-[--lightBlue] font-semibold text-lg'>Features</Link>
+            <Link to="/" className='text-[--lightBlue] font-semibold text-lg'>Get in Touch</Link>
         </div>
         <div className="navbarRightSide">
-            <button className='loginBtn bg-white text-blue-500 py-2 px-6 rounded mx-5 border border-solid border-blue-500'>Log in</button>
-            <button className='registerBtn bg-blue-500 hover:bg-blue-700  py-2 px-6 rounded focus:outline-none focus:ring text-white'>Register</button>
+            <button className='loginBtn bg-white text-[--lightBlue] hover:bg-gray-300 py-2 px-6 rounded mx-5 border border-solid border-[--lightBlue]'>Log in</button>
+            <button className='registerBtn bg-[--lightBlue] hover:bg-[--darkBlue]  py-2 px-6 rounded focus:outline-none focus:ring text-white'>Register</button>
         </div>
 
 
