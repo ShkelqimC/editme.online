ad0ee7c956313799eaafb3411d11ac15aeac6e1b
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2023-04-24 11:51:42 +0200

    navbar color added and changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,16 +4,15 @@ import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <div className="w-screen flex justify-between h-20 items-center shadow-md bg-[--background] px-16" >
-        <div className="navbarLeftSide flex flex-row items-center">
-          
-            <h1 className='text-3xl pr-20'>Logo</h1>
-            <Link to="/" className='px-20 text-[--lightBlue] font-semibold text-lg'>Features</Link>
-            <Link to="/" className='text-[--lightBlue] font-semibold text-lg'>Get in Touch</Link>
+    <div className="w-screen flex justify-between h-20 items-center shadow-md bg-[--primary] px-16" >
+        <div className="navbarLeftSide flex flex-row items-center">          
+            <Link to="/" className='text-3xl pr-20  text-[--onPrimary]'>Logo</Link>
+            <Link to="/edit" className='px-20 text-[--onPrimary]  hover:text-[--onSecondary] font-semibold text-lg'>Editor</Link>
+            <Link to="/getintouch" className='text-[--onPrimary] font-semibold text-lg hover:text-[--onSecondary]'>Get in Touch</Link>
         </div>
         <div className="navbarRightSide">
-            <button className='loginBtn bg-white text-[--lightBlue] hover:bg-gray-300 py-2 px-6 rounded mx-5 border border-solid border-[--lightBlue]'>Log in</button>
-            <button className='registerBtn bg-[--lightBlue] hover:bg-[--darkBlue]  py-2 px-6 rounded focus:outline-none focus:ring text-white'>Register</button>
+            <Link to="/signin" className='loginBtn bg-[--onPrimary] text-[--primary] hover:bg-gray-300 py-2 px-6 rounded mx-5 border border-solid border-[--secondary] hover:bg-[--onSecondary]'>Log in</Link>
+            <Link to="/register" className='registerBtn bg-[--onBackground] hover:bg-[--secondary]  py-2 px-6 rounded focus:outline-none focus:ring text-white'>Register</Link>
         </div>
 
 
