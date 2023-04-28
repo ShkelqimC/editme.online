e8ed264fd0c5dd2e46f704bf99a6c7fa26e4dfa6
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2023-04-28 14:18:00 +0200

    removed title field and made name nullable

--- backend/EditMe.Online/Dtos/AccountDtos/AuthenticateResponse.cs
+++ backend/EditMe.Online/Dtos/AccountDtos/AuthenticateResponse.cs
@@ -3,9 +3,8 @@ namespace EditMe.Online.Dtos.AccountDtos;
 public class AuthenticateResponse
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public string Email { get; set; }
     public string Role { get; set; }
     public DateTime Created { get; set; }
