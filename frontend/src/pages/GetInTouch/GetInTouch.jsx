c3e786c4adfe500f611aca2ad4628b8d581dbb7a
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2023-04-30 20:47:53 +0200

    added recaptcha on contact form

--- frontend/src/pages/GetInTouch/GetInTouch.jsx
+++ frontend/src/pages/GetInTouch/GetInTouch.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import { Link } from "react-router-dom";
-
+import ReCAPTCHA from "react-google-recaptcha";
 const GetInTouch = () => {
+  const [isVerified, setIsVerified] = useState(false);
+  const recaptchaRef = useRef();
+  const form = useRef();
+  // console.log(localStorage.getItem("EditMe-current-theme"))
+
+  const onReSubmit = (value) => {
+    document.getElementById("captchaForm").submit();
+    console.log("Captcha value:", value);
+    setIsVerified(true);
+  };
+  //TODO: recapcha real key when website is live
+  const sendEmail = (e) => {
+    const recaptchaValue = recaptchaRef.current.getValue();
+    this.props.onSubmit(recaptchaValue);
+    e.preventDefault();
+    emailjs
+      // .sendForm("service_23z3i3t", "template_pg2m42m", form.current,"AMTiSuUZ5ifz8P9Dv")
+      .sendForm(
+        `${process.env.REACT_APP_EMAILJS_SERVICEID}`,
+        `${process.env.REACT_APP_EMAILJS_TEMPLATE_ID}`,
+        form.current,
+        `${process.env.REACT_APP_EMAILJS_PUBLIC_KEY}`
+      )
+      .then(
+        function (response) {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+  };
   return (
     <section>
       {/* start intro */}
@@ -19,7 +52,7 @@ const GetInTouch = () => {
             and I recommend it to anyone looking for a fast, reliable, and high-quality image editing solution.
           </p>
         </div>
-          <hr className="border-2 w-full" />
+        <hr className="border-2 w-full" />
       </article>
       {/* endt intro*/}
       {/* Start About Editme */}
@@ -167,11 +200,7 @@ const GetInTouch = () => {
           </div>
           <div className="lg:w-1/2 xl:w-3/5">
             <div className="flex items-center justify-center p-4 md:p-8 lg:p-12">
-              <img
-                src="/img/getintouch-image-1.jpg"
-                alt=""
-                className="rounded-lg shadow-lg sm:min-h-96"
-              />
+              <img src="/img/getintouch-image-1.jpg" alt="" className="rounded-lg shadow-lg sm:min-h-96" />
             </div>
           </div>
         </div>
@@ -191,37 +220,50 @@ const GetInTouch = () => {
           </div>
           <img src="/img/getintouch-image-2.jpg" alt="reach us" className="p-6 w-4/6 mx-auto rounded-lg" />
         </div>
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={sendEmail} ref={form} id="captchaForm">
           <div>
             <label htmlFor="name" className="text-sm">
               Full name
             </label>
-            <input id="name" type="text" placeholder="" className="w-full p-3 rounded dark:bg-gray-800" />
+            <input name="name" type="text" placeholder="" className="w-full p-3 rounded" />
           </div>
           <div>
             <label htmlFor="email" className="text-sm">
               Email
             </label>
-            <input id="email" type="email" className="w-full p-3 rounded dark:bg-gray-800" />
+            <input name="email" type="email" className="w-full p-3 rounded " />
           </div>
           <div>
             <label htmlFor="message" className="text-sm">
               Message
             </label>
-            <textarea id="message" rows="3" className="w-full p-3 rounded dark:bg-gray-800"></textarea>
+            <textarea name="message" rows="3" className="w-full p-3 rounded "></textarea>
           </div>
+          {/* <div className="g-recaptcha" data-sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY} data-callback="onReSubmit"></div> */}
+          <ReCAPTCHA
+            sitekey={`6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI`}
+            onChange={onReSubmit}
+            theme={localStorage.getItem("EditMe-current-theme")}
+            size="normal"
+            ref={recaptchaRef}
+          />
+          This site is protected by reCAPTCHA and the Google
+          <a href="https://policies.google.com/privacy">Privacy Policy</a> and
+          <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+          <br />
           <button
             type="submit"
+            disabled={isVerified===true?true:false}
             className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded border-2 shadow-md hover:text-coral"
           >
             Send Message
           </button>
         </form>
       </article>
-        <hr className="border-2 w-full" />
-      {/*Start  Contact */}
+      <hr className="border-2 w-full" />
+      {/*End  Contact */}
 
-      <article className="py-6 dark:bg-violet-400 dark:text-gray-900">
+      <article className="py-6">
         <div className="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 md:px-24 xl:px-48">
           <h1 className="text-5xl font-bold leading-none text-center">Something totally different</h1>
           <p className="pt-2 pb-8 text-xl font-medium text-center">
@@ -235,45 +277,57 @@ const GetInTouch = () => {
 
       {/*Start  About Developers */}
       <section className="py-6">
-	<div className="container p-4 mx-auto space-y-16 sm:p-10">
-		<div className="space-y-4">
-			<h3 className="text-2xl font-bold leading-none sm:text-5xl">Meet our talented and creative team who brings editme.online to life.</h3>
-			<p className="max-w-2xl ">We are a team of passionate developers who strive to create innovative and user-friendly solutions. Our goal is to make your editing experience as seamless and enjoyable as possible.</p>
-		</div>
-		<div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2">
-			<div className="flex space-x-6">
-				<img alt="" className="flex-1 flex-shrink-0 object-cover h-56 mb-4 bg-center rounded-xl" src="https://source.unsplash.com/240x320/?portrait?0" />
-				<div className="flex flex-col">
-					<h4 className="text-xl font-semibold">Shekelqim Cakaj</h4>
-					<p className="text-sm">Web developer</p>
-					<div className="flex mt-2 space-x-2">				
-						<Link to="https://github.com/ShkelqimC" title="GitHub">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-4 h-4">
-								<path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
-							</svg>
-						</Link>
-					</div>
-				</div>
-			</div>
-			<div className="flex space-x-6">
-				<img alt="" className="flex-1 flex-shrink-0 object-cover h-56 mb-4 bg-center rounded-xl" src="https://source.unsplash.com/240x320/?portrait?1" />
-				<div className="flex flex-col">
-					<h4 className="text-xl font-semibold">Shenol Osman</h4>
-					<p className="text-sm">Web developer</p>
-					<div className="flex mt-2 space-x-2">					
-						<Link to="https://github.com/shenolosman" title="GitHub">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-4 h-4">
-								<path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
-							</svg>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+        <div className="container p-4 mx-auto space-y-16 sm:p-10">
+          <div className="space-y-4">
+            <h3 className="text-2xl font-bold leading-none sm:text-5xl">
+              Meet our talented and creative team who brings editme.online to life.
+            </h3>
+            <p className="max-w-2xl ">
+              We are a team of passionate developers who strive to create innovative and user-friendly solutions. Our goal is to
+              make your editing experience as seamless and enjoyable as possible.
+            </p>
+          </div>
+          <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2">
+            <div className="flex space-x-6">
+              <img
+                alt=""
+                className="flex-1 flex-shrink-0 object-cover h-56 mb-4 bg-center rounded-xl"
+                src="https://source.unsplash.com/240x320/?portrait?0"
+              />
+              <div className="flex flex-col">
+                <h4 className="text-xl font-semibold">Shekelqim Cakaj</h4>
+                <p className="text-sm">Web developer</p>
+                <div className="flex mt-2 space-x-2">
+                  <Link to="https://github.com/ShkelqimC" title="GitHub">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-4 h-4">
+                      <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="flex space-x-6">
+              <img
+                alt=""
+                className="flex-1 flex-shrink-0 object-cover h-56 mb-4 bg-center rounded-xl"
+                src="https://source.unsplash.com/240x320/?portrait?1"
+              />
+              <div className="flex flex-col">
+                <h4 className="text-xl font-semibold">Shenol Osman</h4>
+                <p className="text-sm">Web developer</p>
+                <div className="flex mt-2 space-x-2">
+                  <Link to="https://github.com/shenolosman" title="GitHub">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-4 h-4">
+                      <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       {/*End  About Developers */}
-
     </section>
   );
 };
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./home.css";
 import { DragDropImage } from "../../components/DragDropImage";
 import { Link } from "react-router-dom";
 
