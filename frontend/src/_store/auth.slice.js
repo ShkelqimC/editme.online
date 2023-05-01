68af3ceba544a757843d8ebb2d257821835c6c54
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2023-05-01 16:13:38 +0200

    get in touch change developer side

--- frontend/src/_store/auth.slice.js
+++ frontend/src/_store/auth.slice.js
@@ -10,20 +10,58 @@ const initialState = {
   loading: false,
   auth: user,
 };
+//authenticate user with email and password
 export const login = createAsyncThunk("auth/login", async (payload) => {
   const response = await fetchWrapper.post(`${baseUrl}/authenticate`, payload);
   setToken(response);
   return response;
 });
 
+//refresh token if user is logged in and token is valid and not expired 
+//TODO: this logic should be in middleware 
+//at the moment this logic is using with localstorage
+export const refreshToken = createAsyncThunk("auth/refresh-token", async () => {
+  const response = await fetchWrapper.post(`${baseUrl}/refresh-token`, {});
+  setToken(response);
+  return response;
+});
+
+//forgot password send email
 export const forgotPassword= createAsyncThunk("auth/forgot-password", async (payload) => {
+  const response = await fetchWrapper.post(`${baseUrl}/forgot-password`, payload);
+  return response;
+});
+
+//token sended with email to user, user can reset password
+export const resetPassword = createAsyncThunk("auth/reset-password", async (payload) => {
   const response = await fetchWrapper.post(`${baseUrl}/reset-password`, payload);
   return response;
 });
+
+//revoking token
 export const logout = createAsyncThunk("auth/signOut", async () => {
   var token = JSON.parse(getToken())?.jwtToken;
   await fetchWrapper.post(`${baseUrl}/revoke-token`, { token });
   removeToken();
+});
+
+//register user with email and password
+export const register = createAsyncThunk("auth/register", async (payload) => {
+  const response = await fetchWrapper.post(`${baseUrl}/register`, payload);
+  setToken(response);
+  return response;
+});
+
+//then user get the token
+export const verifyEmail = createAsyncThunk("auth/verify-email", async (payload) => {
+  const response = await fetchWrapper.post(`${baseUrl}/verify-email`, payload);
+  return response;
+});
+
+//if user loose verify token can want new token in user settings page
+export const resendVerifyEmail = createAsyncThunk("auth/resend-verify-email", async (payload) => {
+  const response = await fetchWrapper.post(`${baseUrl}/resend-verify-email`, payload);
+  return response;
 });
 
 export const authSlice = createSlice({
